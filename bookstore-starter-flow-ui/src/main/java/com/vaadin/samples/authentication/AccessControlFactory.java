d02f8dab33c75fb5b3346be87ed8b0839f30c9d5
Mehdi Javan <mehdi@vaadin.com>
2018-07-11 08:57:31 +0300

    making constant name uppercase

--- bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/authentication/AccessControlFactory.java
+++ bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/authentication/AccessControlFactory.java
@@ -1,14 +1,14 @@
 package com.vaadin.samples.authentication;
 
 public class AccessControlFactory {
-    private static final AccessControlFactory instance = new AccessControlFactory();
+    private static final AccessControlFactory INSTANCE = new AccessControlFactory();
     private final AccessControl accessControl = new BasicAccessControl();
 
     private AccessControlFactory() {
     }
 
     public static AccessControlFactory getInstance() {
-        return instance;
+        return INSTANCE;
     }
 
     public AccessControl createAccessControl() {
