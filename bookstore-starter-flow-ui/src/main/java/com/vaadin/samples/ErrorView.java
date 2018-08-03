f8ecb5516e905c6bbaa5c7afd187ff7f141e50b2
Mehdi Javan <mehdi@vaadin.com>
2018-08-03 14:54:18 +0300

    - fixing pom.xml of production module - minor change in ErrorView

--- bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/ErrorView.java
+++ bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/ErrorView.java
@@ -22,7 +22,9 @@ public class ErrorView extends VerticalLayout implements HasErrorParameter<NotFo
     public ErrorView() {
         H1 header = new H1("The view could not be found");
         add(header);
-        add(explanation = new Span());
+
+        explanation = new Span();
+        add(explanation);
     }
 
     @Override
