cfdcc973401f5801811d7185cbb14d452f81330c
Pekka <pekka@vaadin.com>
2018-11-06 16:52:58 +0200

    Use vaadin-checkbox-group instead of addon

--- bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/crud/ProductForm.java
+++ bookstore-starter-flow-ui/src/main/java/com/vaadin/samples/crud/ProductForm.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Locale;
 
-import org.vaadin.pekka.CheckboxGroup;
-
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
@@ -126,8 +126,7 @@ public class ProductForm extends Div {
 
         category = new CheckboxGroup<>();
         category.setId("category");
-        category.getContent().getStyle().set("flex-direction", "column")
-                .set("margin", "0");
+        category.addThemeVariants(CheckboxGroupVariant.LUMO_VERTICAL);
         Label categoryLabel = new Label("Categories");
         categoryLabel.setClassName("vaadin-label");
         categoryLabel.setFor(category);
